d5c2ff36d49e925f47a7c10cdbea33c75e64bb94
Ujjwal1401 <146160614+Ujjwal1401@users.noreply.github.com>
2025-03-03 14:37:04 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_E1ScBSiVq', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '67mql1pvn62bf7m98vkqhgt9nv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_EgvEpszuA', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4gv9t3sfpaf5qsqd4dbb7s28ho', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
